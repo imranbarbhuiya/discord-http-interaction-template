c3a65b2fea293ac3e2c2310b2e6da4816a38ec94
parbez <imranbarbhuiya.fsd@gmail.com>
2024-02-22 16:40:20 +0530

    feat: setup http interaction bot template

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import crypto from 'node:crypto';
 import process from 'node:process';
+import { InteractionResponseType, InteractionType } from 'discord-api-types/v10';
+import { verify } from 'discord-verify';
 import Fastify from 'fastify';
 
 const port = Number(process.env.PORT ?? 3_000);
+const publicKey = process.env.DISCORD_PUBLIC_KEY!;
 
 const fastify = Fastify({
 	logger: true,
@@ -9,8 +13,20 @@ const fastify = Fastify({
 
 fastify.get('/', () => ({ message: 'I am alive' }));
 
-fastify.get('/interaction', async (request, reply) => {
-	await reply.send({ message: request.query });
+fastify.post('/interaction', async (req, res) => {
+	const signature = req.headers['x-signature-ed25519'] as string;
+	const timestamp = req.headers['x-signature-timestamp'] as string;
+	const rawBody = JSON.stringify(req.body);
+
+	const isValid = await verify(rawBody, signature, timestamp, publicKey, crypto.webcrypto.subtle);
+
+	if (!isValid) return res.code(401).send('Bad request signature');
+
+	const { type } = req.body as { data: any; id: string; type: number };
+
+	if (type === InteractionType.Ping) return res.send({ type: InteractionResponseType.Pong });
+
+	await res.send({ message: 'Success' });
 });
 
 await fastify.listen({ port });
